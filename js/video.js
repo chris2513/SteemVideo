3d3d8fbbfcc4bc8b094d6044b40926f23906520b
Chris Osborne <chris@Chris.lan>
2017-08-26 16:46:36 +0100

    youtube fix in extension

--- js/video.js
+++ js/video.js
@@ -1,9 +1,18 @@
 
 $(document).ready(function() {
     $('#videoButton').click(function() {
+        // Get URL from the HTML
         var video = $('#videoURL').val();
+
+        // Clean the URL to work as an embedded player
+        if($('video:contains("watch?v=")')) {
+            video = video.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/" );
+        }
+        // Error checking
         console.log(video);
 
-        $('#video').append('<video src='+video+'></video>');
+        // Inject the youtube player into the HTML
+        $('#video').append('<iframe width="560" height="315" src='+video+' frameborder="0"></iframe>');
     });
+
 });
